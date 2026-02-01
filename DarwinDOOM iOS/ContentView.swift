2bad00b61100b913743412c3b141877df4311b7b
Tanner W. Stokes <tanner@tannr.com>
2026-02-01 10:44:43 -0500

    Maintain aspect ratio on iOS.

--- DarwinDOOM iOS/ContentView.swift
+++ DarwinDOOM iOS/ContentView.swift
@@ -11,18 +11,39 @@ import SpriteKit
 struct ContentView: View {
     @State private var renderCoordinator = DoomRenderCoordinator()
     @State private var keyboardManager = KeyboardInputManager()
+    private static let targetAspect: CGFloat = 640.0 / 400.0
 
     var body: some View {
-        ZStack {
-            SpriteView(scene: renderCoordinator.scene)
-                .ignoresSafeArea()
-            TouchControlOverlay()
+        GeometryReader { proxy in
+            let container = proxy.size
+            let fitted = Self.aspectFitSize(container, aspect: Self.targetAspect)
+
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                SpriteView(scene: renderCoordinator.scene)
+                    .frame(width: fitted.width, height: fitted.height)
+                TouchControlOverlay()
+            }
+            .frame(width: container.width, height: container.height, alignment: .center)
         }
         .onAppear {
             keyboardManager.start()
         }
         .onDisappear {
             keyboardManager.stop()
+        }
+    }
+
+    private static func aspectFitSize(_ size: CGSize, aspect: CGFloat) -> CGSize {
+        guard size.width > 0, size.height > 0, aspect > 0 else { return .zero }
+        let containerAspect = size.width / size.height
+        if containerAspect > aspect {
+            let height = size.height
+            return CGSize(width: height * aspect, height: height)
+        } else {
+            let width = size.width
+            return CGSize(width: width, height: width / aspect)
         }
     }
 }
